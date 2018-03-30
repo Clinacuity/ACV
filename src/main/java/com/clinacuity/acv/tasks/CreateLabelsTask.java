3d6a6b1a506ddfb7924ac66c2649182d002d02ab
JK Accetta <jkaccetta@clinacuity.com>
2018-03-30 17:44:10 -0400

    DEV-194: fixed event; fixed dynamic line length to static

--- src/main/java/com/clinacuity/acv/tasks/CreateLabelsTask.java
+++ src/main/java/com/clinacuity/acv/tasks/CreateLabelsTask.java
@@ -3,34 +3,41 @@ package com.clinacuity.acv.tasks;
 import com.clinacuity.acv.controls.AnnotatedDocumentPane;
 import com.clinacuity.acv.controls.LineNumberedLabel;
 import javafx.concurrent.Task;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import static com.clinacuity.acv.controllers.ConfigurationBuilderController.CorpusType;
 
-public class CreateLabelsFromDocumentTask extends Task<List<LineNumberedLabel>> {
-    private static final Logger logger = LogManager.getLogger();
-    private String[] lines;
-    private List<Label> lineNumbers = new ArrayList<>();
-    public List<Label> getLineNumbers() { return lineNumbers; }
+public class CreateLabelsTask extends Task<Map<CorpusType, List<LineNumberedLabel>>> {
+    private String sysRawText;
+    private String refRawText;
 
-    public CreateLabelsFromDocumentTask(String rawText) {
-        lines = rawText.split("\n");
+    public CreateLabelsTask(String sysRawText, String refRawText) {
+        this.sysRawText = sysRawText;
+        this.refRawText = refRawText;
     }
 
-    @Override public List<LineNumberedLabel> call() {
+    @Override
+    public Map<CorpusType, List<LineNumberedLabel>> call() {
+        Map<CorpusType, List<LineNumberedLabel>> map = new HashMap<>();
+
+        map.put(CorpusType.SYSTEM, getLines(sysRawText));
+        map.put(CorpusType.REFERENCE, getLines(refRawText));
+        updateValue(map);
+        succeeded();
+        return map;
+    }
+
+    private List<LineNumberedLabel> getLines(String rawText) {
+        String[] lines = rawText.split("\n");
         List<LineNumberedLabel> labelList = new ArrayList<>();
 
         // TODO: DEV-24; improve the line-wrapping to break on spaces
-        int maxChars = AnnotatedDocumentPane.getMaxCharactersPerLabel();
+        int maxChars = AnnotatedDocumentPane.MAX_CHARACTERS_PER_LABEL;
         double offset = 0.0d;
-        double offsetIncrement = AnnotatedDocumentPane.getCharacterHeight() * 2.0d;
-        if (offsetIncrement <= 0.0d) {
-            offsetIncrement = 34.0d;
-        }
+        double offsetIncrement = AnnotatedDocumentPane.CHARACTER_HEIGHT * 2.0d;
 
         int runningLength = 0;
         for (int i = 0; i < lines.length; i++) {
@@ -60,7 +67,6 @@ public class CreateLabelsFromDocumentTask extends Task<List<LineNumberedLabel>> 
                     runningLength += subLine.length();
                     line = line.substring(subLine.length());
                 }
-
                 runningLength++;
             } else {
                 LineNumberedLabel label = new LineNumberedLabel(line, i + 1);
